7815303b2093f832f515dd615c0ec8a26c1bf680
Loy Hernandez <loyhernandez59@gmail.com>
2017-12-01 15:51:17 -0600

    more keys

--- app/src/main/java/com/loys/keyboardapp/MyView.java
+++ app/src/main/java/com/loys/keyboardapp/MyView.java
@@ -12,6 +12,10 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.view.MotionEvent;
 
+import static java.lang.Math.*;
+
+import static java.lang.Math.*;
+
 public class MyView extends GestureOverlayView {
 
 
@@ -20,8 +24,9 @@ public class MyView extends GestureOverlayView {
     public MyView(Context c){
         super(c);
         myContext = c;
-        this.setGestureVisible(false);
+        //this.setGestureVisible(false);
         this.setBackgroundColor(Color.WHITE);
+        createKeys();
     }
 
 
@@ -39,20 +44,115 @@ public class MyView extends GestureOverlayView {
         Paint paint = new Paint();
         paint.setAntiAlias(true);
         paint.setColor(Color.BLACK);
-        canvas.drawOval(new RectF(20,40,70,10),paint);
+        paint.setTextSize(30);
+        drawKeyboard(canvas, paint);
     }
 
+    //int y = 100;
+    int bubbleRadius = 7;
+    boolean focused = false;
+    int bigIndex = -1;
+    int focusIndex = -1;
+    int keyWidth = 0;
 
-    float x = 0;
-    float y = 0;
+    private  void drawKeyboard(Canvas c, Paint p){
+        int keysInRow = 7;
+        keyWidth = this.getWidth()/keysInRow;
+        int keyHeight = keyWidth;
+        this.setMinimumHeight(keyHeight*4);
+
+
+        x = new int[keys.length];
+        y = new int[keys.length];
+
+        for(int i=0; i<keys.length; i++){
+            x[i] = (i%keysInRow)*keyWidth+(keyWidth/2);
+            y[i] = (int)(i/keysInRow)*keyHeight+(keyHeight/2);
+
+            c.drawText(keys[i],x[i],y[i],p);
+        }
+
+        for(int i=1; i<7; i++){
+            c.drawLine(keyWidth*i,0,keyWidth*i,this.getHeight(),p);
+        }
+
+        for(int i=1; i<4; i++){
+            c.drawLine(0,keyHeight*i,this.getWidth(),keyHeight*i,p);
+        }
+
+        //drawBubble((int)xTouch,(int)yTouch,20,c,p);
+        //p.setColor(Color.RED);
+        //c.drawText("timePressed: "+timePressed,70,70,p);
+        //p.setColor(Color.BLACK);
+    }
+
+    private int distance(int i){
+        int a = (int) abs(x[i]-xTouch);
+        int b = (int) abs(100-yTouch);
+        int c = (int) sqrt(a*a + b*b);
+
+        return  c;
+    }
+
+    int[] x, y;
+    String[] keys = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","'","_"};
+
+    private  void createKeys(){
+
+    }
+
+    private  boolean touchingKey(int i){
+        int xDistance = (int) abs(x[i]-xTouch);
+        int yDistance = (int) abs(y[i]-yTouch);
+
+        return (xDistance<(keyWidth/2) && yDistance < (keyWidth/2));
+    }
+
+    float xTouch = 0;
+    float yTouch = 0;
+    long timeEntered = System.currentTimeMillis();
+    long timePressed = 0;
+    int currentKeyIndex = -1;
 
     public boolean onTouchEvent(MotionEvent event) {
-        x = event.getX();
-        y = event.getY();
+        xTouch = event.getX();
+        yTouch = event.getY();
 
-        if(event.getAction()== event.ACTION_UP) keyboard.type("a");
+        String output = "-1";
+
+
+        for(int i=0; i<keys.length; i++){
+            if(touchingKey(i)) {
+                if(currentKeyIndex != i){
+                    timeEntered = System.currentTimeMillis();
+                    currentKeyIndex = i;
+                }
+                else{
+                    timePressed = System.currentTimeMillis()-timeEntered;
+                    if(timePressed > 1000){
+                        keyboard.type(keys[i]);
+                        timePressed = 0;
+                        timeEntered = System.currentTimeMillis();
+                    }
+                }
+
+
+                output = keys[i];
+            }
+        }
+
+        if(event.getAction()== event.ACTION_UP) keyboard.type(output);
         invalidate();
         return true;
+    }
+
+    private void drawBubble(int centerX, int centerY, int radius, Canvas c, Paint p){
+        float left = centerX - radius;
+        float top = centerY - radius;
+        float right = centerX + radius;
+        float bottom = centerY + radius;
+
+        c.drawOval(new RectF(left, top, right, bottom), p);
     }
 
 }
